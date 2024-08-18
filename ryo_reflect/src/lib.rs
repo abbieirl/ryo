bebc8649facfb4434f7eb17bac908089d9a53239
lina♡ <38864051+KiraCoding@users.noreply.github.com>
2024-08-18 11:21:27 +0200

    wip

--- ryo_reflect/src/lib.rs
+++ ryo_reflect/src/lib.rs
@@ -10,5 +10,10 @@ pub mod r#struct;
 #[cfg(feature = "rtti")]
 pub mod r#type;
 
+pub mod prelude {
+    pub use crate::reflect::Reflect;
+    pub use crate::r#struct::Struct;
+}
+
 #[cfg(feature = "derive")]
 pub use ryo_reflect_derive::Reflect;
